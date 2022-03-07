f49ebdacd0098a3ba71a09b2b95e18bc948d4af6
sriveraufm <97861517+sriveraufm@users.noreply.github.com>
2022-03-07 12:13:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -148,21 +148,6 @@ def eliminar(nbr):
       arr2 = ordenes  
     return(arr2)
 
-# def menu():
-#   print("-----------------------------------")
-#   print("Seleccione la accion que desea realizar")
-#   print("1. Agregar Orden")
-#   print("2. Realizar Pago")
-#   print("3. Anular Orden")
-#   print("4. Imprimir Ordenes")
-#   print("5. Eliminar Orden")
-#   print("6. Agregar Productos Nuevos al Inventario")
-#   print("8. Ver Inventario")
-#   print("9. Modificar Inventario")
-#   print("10. Realizar Descuentos")
-#   print("11. Cambiar Nombre")#este mejor no
-#   print("0. Salir Del Programa")
-
 def guardar():
   '''Guarda el array multidimensional de ordenes en un archivo csv para conservar los cambios'''
   with open("ordenes.csv", "w") as f:
@@ -195,111 +180,6 @@ def printOrdenes():
     for r in ordenes:
       print( ' '.join([str(x) for x in r] ) ) 
 
-# if(FALSE):
-#   menu()
-#   opcion = int(input("==> "))
-
-#   if(opcion == 1):
-#     print("Agregar ordenes \n")
-#     prodA = input("Producto: ")
-#     May=mayus(prodA)
-#     totalA = int(input("Cantidad: "))
-#     print("\n")
-
-#     idA = uuid.uuid4()
-#     productoInventario = inventario.listfind(prodA)
-#     if(productoInventario is not None):
-#       if(int(productoInventario[2])>=totalA):
-#         total = totalA * float(productoInventario[1])
-#         ordenes.append([j for j in [idA,May,totalA,"PENDIENTE",total]])
-#         # valorPrueba=int(productoInventario[2]) - totalA
-#         # print(valorPrueba)
-#         inventario.listmodify(prodA,str(int(productoInventario[2]) - totalA), 'Inventario')
-#         print("Producto ingresado con exito")
-#         guardar()
-#       else:
-#         print("El producto que desea comprar no cuenta con existencias, lo sentimos.")
-#     else:
-#       print("El producto que desea comprar no existe")
-
-#   if(opcion == 2):
-#     print("Realizando Pago\n")
-#     idR = str(input("NOMBRE: "))
-#     MayR=mayus(idR)
-#     g = 0
-#     for k in range(0,len(ordenes)):
-#       if ordenes[k][1]== MayR:
-#         ordenes[k][3]="PAGADA"
-#         g = 1
-#         print("Producto pagado con exito")
-#         guardar()
-#     if g==0:
-#       print("El producto no ha sido encontrado")
-
-#   if(opcion == 3):#hay que cambiar esto a que busque el id de la orden porque pueden haber varias ordenes para el mismo producto
-#     print("Anulacion de orden\n")
-#     idR = str(input("NOMBRE: "))
-#     MayR=mayus(idR)
-#     g = 0
-#     for k in range(0,len(ordenes)):
-#       if ordenes[k][1]== MayR:
-#         ordenes[k][3]="ANULADA"
-#         g = 1
-#         print("Producto anulada con exito")
-#         guardar()
-#     if g==0:
-#       print("El producto no ha sido encontrado")
-  
-#   if(opcion == 4):
-#     for r in ordenes:
-#       print( ' '.join([str(x) for x in r] ) ) 
-
-#   if(opcion == 5):#igual aca, cambiar a que busque el id
-#     print("Eliminacion de orden\n")
-#     idR = str(input("NOMBRE: "))
-#     MayR=mayus(idR)
-#     ordenes = eliminar(MayR)
-#     guardar()
-    
-#   if(opcion == 6):
-#     print("Agregando nuevos productos \n")
-#     producto = str(input("Producto que desea agregar: "))
-#     precio = float(input("Precio del producto: "))
-#     inventario = int(input("Inventario disponible actual: "))
-#     if(inventario.listfind(producto) is not None):
-#       print("Este producto ya existe")
-#     else:
-#       if(inventario.headval.nextval is None):
-#         inventario.agregar([producto, precio, inventario])
-#       else:
-#         inventario.agregar([producto, precio, inventario])
-
-#   if(opcion == 8):
-#     print('INVENTARIO \n')
-#     inventario.listprint()
-
-#   if(opcion == 9):
-#     pass
-
-#   if(opcion == 10):
-#     print("Realizar Descuentos \n")
-#     prodA = input("Nombre del producto: ")
-#     May=mayus(prodA)
-#     precioA = int(input("Nuevo precio: "))
-#     print("\n")
-#     inventario.listmodify(prodA,precioA, 'Precio')
-
-#   if(opcion == 11):
-#     print("Cambiar Nombre \n")
-#     prodA = input("Nombre del producto: ")
-#     May=mayus(prodA)
-#     nombreA = str(input("Nuevo Nombre: "))
-#     print("\n")
-#     inventario.listmodify(prodA,nombreA, 'Producto')
-    
-#   # if(opcion == 0):
-    
-#   #   break
   
 
 
