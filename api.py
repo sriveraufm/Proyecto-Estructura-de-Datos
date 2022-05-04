996851085793a630655aebc1bf5bfb9d6c647b48
Javier Caniz <javier caniz@ufm.edu>
2022-05-03 20:29:33 -0600

    queue

--- api.py
+++ api.py
@@ -193,7 +193,7 @@ def inventarioAgregrarAPI():
     Tome en cuenta que el API no permite agregar un producto que ya exista'''
     orden = {'PRODUCTO' : request.json['producto'],'PRECIO' : request.json['precio'], 'INVENTARIO': request.json['inventario']}
     producto = mayus(str(list(orden.values())[0]))
-    BPlusTreeV2.bplustree.insert(producto, producto)
+    BPlusTreeV2.bplustree.insert(str(producto), str(producto))
     BPlusTreeV2.bplustree.show()
     precio = float(list(orden.values())[1])
     inv = int(list(orden.values())[2])
